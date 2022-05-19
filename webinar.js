77a52126481ea9603af7b41a8543702581d99f92
Egor Vinogradov <1618344+egorvinogradov@users.noreply.github.com>
2022-05-19 20:18:56 +0600

    Update webinar script

--- webinar.js
+++ webinar.js
@@ -1,99 +1,119 @@
 /*
 
-<div class="bc-webinar">
-  <iframe id="bc-player"
-    src="https://www.youtube.com/embed/1bFieQsV5TM?enablejsapi=1"
-    allow="fullscreen;"
-    allowfullscreen
-    frameborder="0"></iframe>
-</div>
-
+<div class="bc-webinar"></div>
 <link rel="stylesheet" href="https://believencode.surge.sh/webinar.css"/>
-<script src="https://www.youtube.com/iframe_api"></script>
 <script src="https://believencode.surge.sh/webinar.js"></script>
+<script src="https://www.youtube.com/iframe_api"></script>
 
 */
 
 
-window.BC = window.BC || {};
-window.BC.client = getClientData();
-window.BC.youtubeAPILoaded = false;
+const BC = window.BC || {};
+BC.params = getPageParams();
+BC.youtubeAPILoaded = false;
 
 
-if (window.BC.client.lead_id) {
-  changeAmoStatus(window.BC.client.lead_id, 'opened_webinar');
+if (BC.params.lead_id) {
+  changeAmoStatus(BC.params.lead_id, 'opened_webinar');
 }
 
+setContactFormCourse(BC.params.course_type);
 
-
-// onYouTubeIframeAPIReady() is supposed to be fired by Youtube JS API
-// but it is not working reliably
-// therefore it could either be fired by a timeout (below)
-
-function onYouTubeIframeAPIReady(){
-  console.log('onYouTubeIframeAPIReady fired');
-  window.BC.youtubeAPILoaded = true;
-  let isOnReadyFired = false;
-
-  window.BC.webinar = new YT.Player('bc-player', {
-    events: {
-      onReady: () => {
-        if (!isOnReadyFired) {
-          isOnReadyFired = true;
-          console.log('onWebinarReady fired');
-          onWebinarReady();
-        }
-      },
-    },
-  });
-
-  setTimeout(() => {
-    if (!isOnReadyFired) {
-      isOnReadyFired = true;
-      console.warned('(!) onWebinarReady fired by timeout');
-      onWebinarReady();
-    }
-  }, 10 * 1000);
-}
-
-
-
-setTimeout(() => {
-  if (!window.BC.youtubeAPILoaded) {
-    console.warn('(!) onYouTubeIframeAPIReady fired by timeout');
-    onYouTubeIframeAPIReady();
-  }
-}, 15 * 1000);
-
-
-
-function onWebinarReady(){
-  console.log('Webinar ready', window.BC);
-  window.BC.webinarDuration = BC.webinar.getDuration();
+waitUntilWebinarIsReady(BC.params.course_type, (webinar) => {
+  BC.webinar = webinar;
+  BC.webinarDuration = BC.webinar.getDuration();
+  console.log('Webinar ready');
   enableWebinarTracker();
+});
+
+
+// Youtube JS API 'onReady' event is unreliable
+// therefore using intervals to readiness checking
+function waitUntilWebinarIsReady(course_type, callback){
+  renderVideo(course_type);
+
+  let isYoutubeAPIReady = false;
+  let isVideoReady = false;
+  let webinar = null;
+
+  const interval = setInterval(() => {
+    if (!isYoutubeAPIReady && window.YT && window.YT.Player) {
+      webinar = new YT.Player('bc-player');
+      isYoutubeAPIReady = true;
+    }
+    if (!isVideoReady && webinar && webinar.getDuration) {
+      isVideoReady = true;
+    }
+    console.log('Checking webinar status... API: ' + isYoutubeAPIReady + '; video: ' + isVideoReady);
+
+    if (isYoutubeAPIReady && isVideoReady) {
+      clearInterval(interval);
+      callback(webinar);
+    }
+  }, 300);
+}
+
+
+function getYoutubeVideoId(course_type){
+  // noinspection NonAsciiCharacters,JSNonASCIINames
+  const ids = {
+    'Фронтенд': '1bFieQsV5TM',
+    'UI/UX-дизайн': '1bFieQsV5TM',
+    'Графический дизайн': '1bFieQsV5TM',
+  };
+  return ids[course_type || 'Фронтенд'];
+}
+
+
+function getWebinarTitle(course_type){
+  // noinspection NonAsciiCharacters,JSNonASCIINames
+  const titles = {
+    'Фронтенд': 'Вебинар курса по фронтенд-разработке (JavaScript/ReactJS)',
+    'UI/UX-дизайн': 'Вебинар курса по UI/UX-дизайну в Figma',
+    'Графический дизайн': 'Вебинар курса по графическому дизайну в Photoshop',
+  };
+  return titles[course_type || 'Фронтенд'];
+}
+
+
+function setContactFormCourse(course_type) {
+  document.querySelector('.uc-contact-form-full select[name=course]').value = course_type;
+}
+
+
+function renderVideo(course_type){
+  const videoId = getYoutubeVideoId(course_type);
+  const title = getWebinarTitle(course_type);
+  const container = document.querySelector('.bc-webinar');
+
+  container.innerHTML = '<h1 class="bc-webinar-header t-section__title t-title t-title_xs">' + title + '</h1>'
+    + '<iframe id="bc-player"'
+    + '  src="https://www.youtube.com/embed/' + videoId + '?enablejsapi=1"'
+    + '  allow="fullscreen;"'
+    + '  allowfullscreen'
+    + '  frameborder="0"></iframe>';
 }
 
 
 function enableWebinarTracker(){
-  console.log('Enable webinar tracker', window.BC);
-  const lead_id = window.BC.client.lead_id;
-
-  window.BC.webinarTracker = setInterval(() => {
+  console.log('Enable webinar tracker', BC);
+  const lead_id = BC.params.lead_id;
+  const interval = setInterval(() => {
     trackWebinarCompletion(() => {
-      clearInterval(window.BC.webinarTracker);
-      showOffer(lead_id);
+      clearInterval(interval);
 
       if (lead_id) {
         changeAmoStatus(lead_id, 'watched_webinar');
       }
+      showOffer(lead_id);
     });
   }, 1000);
 }
 
 
 function trackWebinarCompletion(callback){
-  const time = window.BC.webinar.getCurrentTime();
-  const percentage = time / window.BC.webinarDuration;
+  const time = BC.webinar.getCurrentTime();
+  const percentage = time / BC.webinarDuration;
 
   console.log('Webinar completion: ' + Math.floor(percentage * 100) + '%');
 
@@ -109,13 +129,13 @@ function showOffer(lead_id){
 
 
 function changeAmoStatus(lead_id, status){
-  const webhookUrl = 'https://hook.eu1.make.com/xqdq4hhnhdca8ds1ranjgpgyxg9j68ey';
+  const webHookUrl = 'https://hook.eu1.make.com/xqdq4hhnhdca8ds1ranjgpgyxg9j68ey';
   const paramString = '?lead_id=' + lead_id + '&to_status=' + status;
 
   console.log('Amo status: triggering webhook: ' + paramString);
 
-  return fetch(webhookUrl + paramString).then(() => {
-    console.log('Amo status: succesfully triggered webhook');
+  return fetch(webHookUrl + paramString).then(() => {
+    console.log('Amo status: successfully triggered webhook');
   });
 }
 
@@ -133,15 +153,17 @@ function parseGetParams(){
 }
 
 
-function getClientData(){
-  let params = parseGetParams();
-  let lead_id = params.lead_id || localStorage.getItem('lead_id');
+/**
+ * @return {{ lead_id: string, course_type: string }}
+ */
+function getPageParams(){
+  const keys = ['lead_id', 'course_type'];
+  const getParams = parseGetParams();
+  const params = {};
 
-  if (lead_id) {
-    localStorage.setItem('lead_id', lead_id);
-  }
+  keys.forEach(key => {
+    params[key] = getParams[key] || localStorage.getItem(key);
+  });
 
-  return {
-    lead_id: lead_id,
-  };
+  return params;
 }
